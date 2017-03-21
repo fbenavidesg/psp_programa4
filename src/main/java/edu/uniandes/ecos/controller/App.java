aa67da201963fe5a7fc049d3a7d4befb04815e8f
Fredy Benavides Gordillo <f.benavides@uniandes.edu.co>
2017-03-20 20:27:54 -0500

    prueba 3

--- src/main/java/edu/uniandes/ecos/controller/App.java
+++ src/main/java/edu/uniandes/ecos/controller/App.java
@@ -5,9 +5,11 @@
  */
 package edu.uniandes.ecos.controller;
 
-
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
 import static spark.Spark.*;
-
+import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  *
@@ -18,6 +20,10 @@ public class App {
     public static void main(String[] args) {
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
-        get("/", (req, res) -> "Hola mundo");
+        get("/", (req, res) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
     }
 }
